690bd0b1539eac315029247089095c187a03453b
rragrawal <nilesh@MACs-Mac-3.local>
2014-06-18 15:39:01 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  EmptyApp
+//
+//  Created by rragrawal on 27/05/14.
+//  Copyright (c) 2014 Systango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   
+    
+    }
+}
